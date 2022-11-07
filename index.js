5ca26208dd62eda243c27a646d77db8e4301ab2a
forestlet <hyf2021@outlook.com>
2022-11-07 19:08:00 +0800

    remove express

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static("."));
-
-app.listen(3000, function () {
-  //在3000端口启动
-  console.info("复制打开浏览器 http://localhost:3000/");
-});
